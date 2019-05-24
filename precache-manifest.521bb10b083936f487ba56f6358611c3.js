2ca6f4997c68fab4674e6fef71d800748de0f414
Daniel Mantilla Ochoa <danielmomusic@gmail.com>
2019-05-24 18:42:01 -0500

    Updates

---
+++ precache-manifest.521bb10b083936f487ba56f6358611c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f78343f248c32fbe7af714b8d2440fbb",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "1664313a5a804c968f6b",
+    "url": "/repository-name/static/css/main.55502467.chunk.css"
+  },
+  {
+    "revision": "40364105668ad8677986",
+    "url": "/repository-name/static/js/2.06bb692e.chunk.js"
+  },
+  {
+    "revision": "1664313a5a804c968f6b",
+    "url": "/repository-name/static/js/main.ab5a5d43.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  },
+  {
+    "revision": "4d0c761fe97885e97e379b41c506c689",
+    "url": "/repository-name/static/media/coding.4d0c761f.jpg"
+  },
+  {
+    "revision": "07c02484f0cac1b1f1ac72c1611f82ac",
+    "url": "/repository-name/static/media/logo_transparent.07c02484.png"
+  },
+  {
+    "revision": "471a1ad342659289433e05a611d206f8",
+    "url": "/repository-name/static/media/userImage.471a1ad3.png"
+  }
+]);
