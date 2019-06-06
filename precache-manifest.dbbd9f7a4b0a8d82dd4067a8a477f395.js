420ea9554a44aac94ded4c160d117e6b287b5763
Daniel Mantilla Ochoa <danielmomusic@gmail.com>
2019-06-06 06:59:52 -0500

    Updates

---
+++ precache-manifest.dbbd9f7a4b0a8d82dd4067a8a477f395.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f76913f20d866569dac45e4779c7e4",
+    "url": "/social-dev/index.html"
+  },
+  {
+    "revision": "50cabf671bef6d5bdd72",
+    "url": "/social-dev/static/css/main.e69e12f1.chunk.css"
+  },
+  {
+    "revision": "f167bcc2ef3262735104",
+    "url": "/social-dev/static/js/2.314f8e27.chunk.js"
+  },
+  {
+    "revision": "50cabf671bef6d5bdd72",
+    "url": "/social-dev/static/js/main.829e1c90.chunk.js"
+  },
+  {
+    "revision": "170c962aded87d35ebb4",
+    "url": "/social-dev/static/js/runtime~main.33dd46dc.js"
+  },
+  {
+    "revision": "4d0c761fe97885e97e379b41c506c689",
+    "url": "/social-dev/static/media/coding.4d0c761f.jpg"
+  },
+  {
+    "revision": "07c02484f0cac1b1f1ac72c1611f82ac",
+    "url": "/social-dev/static/media/logo_transparent.07c02484.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/social-dev/precache-manifest.8acff607fa8c57c3e2f82a2dbf3d7d62.js"
+  "/social-dev/precache-manifest.dbbd9f7a4b0a8d82dd4067a8a477f395.js"
 );
 
 self.addEventListener('message', (event) => {
